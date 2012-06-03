40df49e353bd440c7447c4282be03b42c7207e58
Massimo DiPierro <massimo.dipierro@gmail.com>
2012-06-03 10:00:06 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding:Utf-8 -*-
+
+from setuptools import setup
+
+setup(name='workflow',
+      version='0.1',
+      description='a minimalist file-based workflow system',
+      author='Massimo DiPierro',
+      author_email='massimo.dipierro@gmail.com',
+      long_description=open("README.md").read(),
+      url='https://github.com/mdipierro/workflow',
+      install_requires=[],
+      py_modules=["workflow"],
+      license= 'BSD',
+      keywords='workflow',
+     )
